bd60ce5734041b32f14a9de32e8f696064b119ed
gary-codes <johncliffordhabab856@gmail.com>
2022-12-08 18:58:50 +0800

    Initial Release

---
+++ document.js
@@ -0,0 +1,15 @@
+function myfunction() {
+    location.href = "next.html";
+}
+
+var b = document.querySelector(".hindi");
+b.addEventListener("click", change);
+
+function change(){
+    var i=Math.floor(Math.random()*400)+3 ;
+    var j=Math.floor(Math.random()*400)-1 ;
+
+    b.style.left=i+"px"
+    b.style.top=j+"px"
+    b.style.position="absolute";
+}
